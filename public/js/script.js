d49a3e53da0faa065486a67345a6c6458c298c68
Isak410 <Isakstenep@gmail.com>
2023-10-23 13:57:59 +0200

    AWFBA

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ const usernameButton = document.getElementById('usernamebutton')
 const usernameInput = document.getElementById('usernameinput')
 const messageDiv = document.getElementById('messages')
 const userInterfaceDiv = document.getElementById('userInterface')
+const usersConnected = document.getElementById('usersCon')
 var myId = -1
 console.log("script loaded")
     var socket = io();
@@ -13,21 +14,21 @@ console.log("script loaded")
     var input = document.getElementById('m');
 
     function submitUsername() {
-      if (usernameInput.value != "") {
+      if (usernameInput.value == "") {return}
       formDiv.style.pointerEvents = "all"
       formDiv.style.opacity = "100%"
       userInterfaceDiv.style.opacity = "100%"
       loginDiv.style.pointerEvents = "none"
       loginDiv.style.opacity = "0%"
       socket.emit('newUser', [myId, usernameInput.value])
-    }
   }
     form.addEventListener('submit', function (e) {
       e.preventDefault(); // Prevent the default form submission behavior
-      
+      if (input.value == "") {return}
       let tid = getTime()  
       let currentTime = (""+tid[0]+":"+tid[1]+":"+tid[2])
-      socket.emit('chat message', {senderID: myId, timeOfSend:currentTime, message: input.value});
+      var msgval = input.value
+      socket.emit('chat message', {senderID: myId, timeOfSend:currentTime, message: msgval});
       input.value = '';
     });
 
@@ -56,25 +57,37 @@ console.log("script loaded")
       userInterfaceDiv.scrollTop = userInterfaceDiv.scrollHeight;}
     });
 
+    socket.on('display_usersconnected', function (int) {
+      usersConnected.textContent = (int+" users connected")
+    })
+
+    socket.on('reload_page', function() {
+      location.reload()
+      console.log("reloading page...")
+    })
+
     var prevTime = [0,0]
     socket.on('chat message', function (msg) {
       console.log(prevTime)
-      if (prevTime[0] != msg.timeOfSend[0]+msg.timeOfSend[1] || prevTime[1] != msg.timeOfSend[3]+msg.timeOfSend[4]){
-        console.log(msg.timeOfSend[0]+msg.timeOfSend[1]+":"+msg.timeOfSend[3]+msg.timeOfSend[4])
+      if (prevTime[0] != msg.timeOfSend[0] || prevTime[1] != msg.timeOfSend[1]){
+        console.log(msg.timeOfSend[0]+":"+msg.timeOfSend[1])
         var timeStamp = document.createElement("p")
         var timeStampDiv = document.createElement("div")
         timeStampDiv.id = "timeStampDiv"
         timeStamp.id = "timeStamp"
-        timeStamp.textContent = (msg.timeOfSend[0]+msg.timeOfSend[1]+":"+msg.timeOfSend[3]+msg.timeOfSend[4])
+        timeStamp.textContent = (msg.timeOfSend[0]+":"+msg.timeOfSend[1])
         console.log("timeStamp generated - "+msg.timeOfSend)
         timeStampDiv.appendChild(timeStamp)
         messageDiv.appendChild(timeStampDiv)
       }
       if (!msg.senderID == myId) {playNotification()}
       var message1 = document.createElement('p');
-      prevTime[0] = msg.timeOfSend[0]+msg.timeOfSend[1]
-      prevTime[1] = msg.timeOfSend[3]+msg.timeOfSend[4]
-      message1.textContent = (msg.user + " - " + msg.message);
+      prevTime[0] = msg.timeOfSend[0]
+      prevTime[1] = msg.timeOfSend[1]
+      var br = document.createElement("br")
+      message1.innerHTML = ("User: "+msg.user);
+      message1.appendChild(br)
+      message1.appendChild(document.createTextNode(msg.message))
       message1.id = msg.senderID
       console.log("my id: "+myId)
       
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const session = require('express-session')
 var usercount = 0
+var usersCurrentlyConnected = 0
 var users = {}
 
 const { Server } = require('socket.io');
@@ -14,21 +16,28 @@ app.get('/', (req, res) => {
 
 app.use(express.json())
 
+
+
 io.on('connection', (socket) => {
     console.log('a user connected');
     io.emit('assinguserid', usercount)
     const currentUserId = usercount;
     usercount++;
-
+    
     socket.on('disconnect', function () {
+        usersCurrentlyConnected--
+        if (usersCurrentlyConnected < 0) {usersCurrentlyConnected = 0}
         console.log(`user ${currentUserId} disconnected`);
         io.emit('user_disconnect', users[currentUserId]);
+        io.emit('display_usersconnected', usersCurrentlyConnected)
         delete users[currentUserId];
     });
 
     socket.on('newUser', (userInfo) => {
+        usersCurrentlyConnected++
         users[currentUserId] = userInfo[1];
         io.emit('userhasconnected', users[currentUserId])
+        io.emit('display_usersconnected', usersCurrentlyConnected)
     });
 
     socket.on('chat message', (msg) => {
@@ -36,6 +45,8 @@ io.on('connection', (socket) => {
         console.log(msg.senderID);
         msg.user = users[msg.senderID];
         console.log(msg.timeOfSend);
+        var sendTimeArr = [msg.timeOfSend[0]+msg.timeOfSend[1],msg.timeOfSend[3]+msg.timeOfSend[4]]
+        msg.timeOfSend = sendTimeArr
         io.emit('chat message', msg);
     });
 });
@@ -46,3 +57,8 @@ app.use(express.static('public'));
 server.listen(8080, () => {
     console.log('Listening on 8080');
 });
+
+setTimeout(()=> {
+    io.emit('reload_page')}
+ ,100);
+
